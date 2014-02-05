16515c5b998c985d50a608894ae3ad599222acf8
Sandeep <sandeep@techaddict.me>
2014-02-05 17:41:42 +0530

    Ray of Hope

--- src/main/scala/me/techaddict/sysmon/SysMonitorServlet.scala
+++ src/main/scala/me/techaddict/sysmon/SysMonitorServlet.scala
@@ -2,16 +2,14 @@ package me.techaddict.sysmon
 
 import org.scalatra._
 import scalate.ScalateSupport
+import scala.sys.process._
 
 class SysMonitorServlet extends SysmonitorStack {
-
+  val x = Process("/bin/df", Seq("-h"))
+  val y = Process("awk", Seq("{print $1\", \"$2\", \"$3\", \"$4\", \"$5\", \"$6}"))
+  val z = x #| y
+  val out = z.lines_!.toArray
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+    jade("/index", "out" -> out.mkString("\n"))
   }
-  
 }

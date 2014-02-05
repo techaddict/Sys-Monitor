48b2a7d13f57f5d553a7bae6023a2002f26134a6
Sandeep <sandeep@techaddict.me>
2014-02-05 18:44:52 +0530

    wip

--- src/main/scala/me/techaddict/sysmon/SysMonitorServlet.scala
+++ src/main/scala/me/techaddict/sysmon/SysMonitorServlet.scala
@@ -2,14 +2,9 @@ package me.techaddict.sysmon
 
 import org.scalatra._
 import scalate.ScalateSupport
-import scala.sys.process._
 
 class SysMonitorServlet extends SysmonitorStack {
-  val x = Process("/bin/df", Seq("-h"))
-  val y = Process("awk", Seq("{print $1\", \"$2\", \"$3\", \"$4\", \"$5\", \"$6}"))
-  val z = x #| y
-  val out = z.lines_!.toArray
   get("/") {
-    jade("/index", "out" -> out.mkString("\n"))
+    jade("/index", "DiskUsage" -> scripts.DiskUsage.value)
   }
 }
---
+++ src/main/scala/me/techaddict/sysmon/scripts/DiskUsage.scala
@@ -0,0 +1,11 @@
+package me.techaddict.sysmon.scripts
+
+import scala.sys.process._
+
+object DiskUsage{
+  private def x = Process("/bin/df", Seq("-h"))
+  private def y = Process("awk", Seq("{print $1\", \"$2\", \"$3\", \"$4\", \"$5\", \"$6}"))
+  private def z = x #| y
+  private def out = z.lines_!.toArray
+  def value = out.mkString("\n")
+}

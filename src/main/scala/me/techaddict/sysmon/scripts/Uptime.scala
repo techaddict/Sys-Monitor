e4f7d29ce270fdb62ee587612770d451d1be7d69
Sandeep <sandeep@techaddict.me>
2014-02-07 14:19:30 +0530

    Internal and External IP

--- src/main/scala/me/techaddict/sysmon/scripts/Uptime.scala
+++ src/main/scala/me/techaddict/sysmon/scripts/Uptime.scala
@@ -5,5 +5,5 @@ import scala.sys.process._
 object Uptime{
   private def x = Process("cat", Seq("/proc/uptime"))
   private def out = x.lines_!.toArray
-  def value = out(0).split(" ")(0)
+  def value = out(0).split(" ")(0) + " Seconds"
 }
